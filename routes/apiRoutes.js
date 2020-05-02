b9542a37236b4ce6462da7e6aec1d0a6deffce66
Moreno Gomes <moreno.gomes@mail.utoronto.ca>
2020-05-02 00:20:06 -0400

    addNotes function fixed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,14 @@
-// const storage = require("../db/storage");
 const router = require("express").Router();
 const storage = require("../db/storage");
-// const uuidv4 = require('uuid/v4');
 
-// const router = ("express").Router();
-// const app = ('express').Router();
-//functionality to for getting notes from your 
-// app.get("/notes", function (req, res) {
+
 router.get("/notes", function (req, res) {
     storage
     .getNotes()
     .then(note => res.json(note))
     .catch(err => res.status(500).json(err));
 });
-//post similiar build to add notes
-// app.post("/notes", function (req, res) {
+
 router.post("/notes", function (req, res) {
     storage
         .addNotes(req.body)
@@ -31,7 +25,6 @@ router.delete("/notes:id", function (req, res) {
 });
 
 
-//delete to delete notes
 
 module.exports = router;
 
